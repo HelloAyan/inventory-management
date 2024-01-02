70c7db86bdd7e60cbd1039686a7fbee9dd87be85
HelloAyan <18103373@iubat.edu>
2024-01-02 21:27:24 +0600

    Category migration and model added

--- resources/views/admin/allcategory.blade.php
+++ resources/views/admin/allcategory.blade.php
@@ -5,5 +5,38 @@
 @endsection
 
 @section('content')
-    hello from all category
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page /</span> All Category</h4>
+        <div class="card">
+            <h5 class="card-header">Available Category</h5>
+
+            <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <thead class="table-light">
+                        <tr>
+                            <th>SL</th>
+                            <th>Category Name</th>
+                            <th>Sub Category</th>
+                            <th>Product</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+
+                        <tr>
+                            <td>{{ $counter }}</td>
+                            <td>{{ $category->category_name }} </td>
+                            <td>{{ $category->subcategory_count }}</td>
+                            <td>{{ $category->product_count }}</td>
+                            <td>
+                                <a href="#" class="btn btn-primary">Edit</a>
+                                <a href="#" class="btn btn-warning">Delete</a>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
